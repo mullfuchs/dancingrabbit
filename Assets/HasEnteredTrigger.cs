2f8502fe847efaf2b88d1eed7ae6c2e0dc07ff6e
mullfuchs <burke.leo@gmail.com>
2017-02-21 22:22:39 -0800

    made some gameplay changes

---
+++ Assets/HasEnteredTrigger.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HasEnteredTrigger : MonoBehaviour {
+
+	public bool enteredTrigger = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	void OnTriggerEnter(){
+		enteredTrigger = true;
+	}
+
+	void OnTriggerExit(){
+		enteredTrigger = false;
+	}
+}
---
+++ Assets/LightAndMusicControls.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightAndMusicControls : MonoBehaviour {
+
+	public GameObject switchObject;
+	public GameObject lightObject;
+
+
+	private AudioSource music;
+	private float timer;
+	private bool isActive = false;
+
+	// Use this for initialization
+	void Start () {
+		resetTimer ();
+		music = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer -= Time.realtimeSinceStartup;
+		if (timer <= 0 && !isActive) {
+			activateBox ();
+		}
+
+		if (switchObject.GetComponent<HasEnteredTrigger> ().enteredTrigger) {
+			deactivateBox ();
+		}
+	}
+
+	void activateBox(){
+		isActive = true;
+		music.Play ();
+		lightObject.SetActive (true);
+	}
+
+	void deactivateBox(){
+		music.Pause ();
+		lightObject.SetActive (false);
+		resetTimer ();
+		isActive = false;
+	}
+
+	void resetTimer(){
+		timer = Random.Range (500, 600);
+	}
+}
