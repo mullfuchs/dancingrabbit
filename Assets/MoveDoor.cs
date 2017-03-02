ed690f6efa1d88134c755568581f80f39ad8a4fc
mullfuchs <burke.leo@gmail.com>
2017-03-01 21:02:49 -0800

    final stuff for the night

--- Assets/MoveDoor.cs
+++ Assets/MoveDoor.cs
@@ -2,16 +2,17 @@
 using System.Collections;
 
 public class MoveDoor : MonoBehaviour {
-	public Transform DoorTarget;
+	public Transform OpenPosition;
 	private bool isOpen = false;
+	private Transform DefaultPosition;
 	// Use this for initialization
 	void Start () {
-	
+		DefaultPosition = this.transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 
 
