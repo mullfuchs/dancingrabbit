21d20336b127232bd2da72cd8597c224e37bf1f9
mullfuchs <burke.leo@gmail.com>
2017-03-01 20:55:21 -0800

    door stuff

---
+++ Assets/MoveDoor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveDoor : MonoBehaviour {
+	public Transform DoorTarget;
+	private bool isOpen = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+
+}
